26dcb6d7dc213de908d9e80799e3062b826ed695
juliogomes92 <julio_gomes92@hotmail.com>
2020-08-04 21:46:08 -0300

    correção de react-router-dom

--- src/components/pageHeader/index.tsx
+++ src/components/pageHeader/index.tsx
@@ -13,7 +13,7 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
         <header className="page-header">
                 <div className="top-bar-container">
                     <Link to="/">
-                    <img src={backIcon} alt="Voltar"/>
+                    <img src={backIcon} alt="Voltar agora"/>
                     </Link>
                     <img src={logoImg} alt="Logo"/>
                 </div>
